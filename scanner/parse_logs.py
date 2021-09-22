fe3b3f26cbf2e4a2de3b6f68ffb934cae818569c
Peter Stephens <code@diligentsoftware.com>
2021-09-21 23:25:48 -0500

    Finish analysis scripts

--- scanner/parse_logs.py
+++ scanner/parse_logs.py
@@ -20,6 +20,17 @@ FIELDS = ["Deg", "Time",
     # AT+QRSRP
     "PRX", "DRX", "RX2", "RX3",
     # AT+QCAINFO / carrier aggregation
+    "CA", "CA Cnt", "CA Tot Bandwidth",
+    # AT+QNWPREFCFG="mode_pref"
+    "Mode Pref",
+    # AT+CREG?
+    "Reg State",
+    # AT+COPS?
+    "Oper", "AcT",
+    # AT+QENDC
+    "5G Icon",
+    # Ping stats
+    "Ping Pkt Loss", "Ping Min", "Ping Max", "Ping Avg", "Ping Jitter"
     ]
 
 SECTION_PATT = re.compile(r"^=+$")
@@ -36,6 +47,20 @@ QENG_LTE_PATT =   re.compile(r"^\d+:\s*\+QENG:\s*\"servingcell\",\s*\"([^\"]+)\"
 QENG_WCDMA_PATT = re.compile(r"^\d+:\s*\+QENG:\s*\"servingcell\",\s*\"([^\"]+)\",\"(WCDMA)\".*$")
 
 QRSRP_PATT = re.compile(r"^\d+:\s*\+QRSRP:\s*(-?\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+)")
+
+QCAINFO_PCC_PATT = re.compile(r"^\d+:\s*\+QCAINFO:\s*\"(PCC)\",\s*(\d+),\s*(\d+),\s*\"LTE BAND (\d+)\",\s*(\d+),\s*(\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+)\s*$")
+QCAINFO_SCC_PATT = re.compile(r"^\d+:\s*\+QCAINFO:\s*\"(SCC)\",\s*(\d+),\s*(\d+),\s*\"LTE BAND (\d+)\",\s*(\d+),\s*(\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+),\s*(-?\d+)\s*$")
+
+MODEPREF_PATT = re.compile(r"^\d+:\s*\+QNWPREFCFG:\s*\"mode_pref\",([A-Z0-9]+)")
+
+CREG_PATT = re.compile(r"^\d+:\s*\+CREG:\s*\d+,\s*(\d+)")
+
+COPS_PATT = re.compile(r"^\d+:\s*\+COPS:\s*\d+,\d+,\"([^\"]+)\",(\d+)")
+
+QENDC_PATT = re.compile(r"^\d+:\s*\+QENDC:\s*\d+,\d+,\d+,(\d+)")
+
+PING_1_PATT = re.compile(r".*bytes from.*time=([0-9.]+) ms")
+PING_2_PATT = re.compile(r".* (\d+)% packet loss.*")
 
 
 def check_patt(line, patt, acc, fields):
@@ -73,7 +98,37 @@ def map_lte_bandwidth(v):
     return LTE_BANDWIDTH_VALS[int(v)]
 
 
+PCELL_STATE = ["No Serving", "Registered"]
+def map_pcell_state(v):
+    return PCELL_STATE[int(v)]
+
+
+SCELL_STATE = ["Deconfigured", "Configured Deactivate", "Configured Activated"]
+def map_scell_state(v):
+    return SCELL_STATE[int(v)]
+
+
+def map_scc_bandwidth(v):
+    v = int(v)
+    if v == 6:
+        return 1.4
+    else:
+        return v / 5
+
+
+CREG_STAT = ["Not Registered", "Registered Home Network", "Registration Denied", "Unknown", "Registered Roaming"]
+def map_creg_state(v):
+    return CREG_STAT[int(v)]
+
+ACT_STATE = [None, None, "UTRAN", None, "UTRAN W/HSDPA", "UTRAN W/HSUPA", "UTRAN W/HSDPA&HSUPA", "E-UTRAN",
+             None, None, "E-UTRAN 5GCN", "NR 5GCN", "NG-RAN", "E-UTRAN-NR dual connect"]
+def map_act_state(v):
+    return ACT_STATE[int(v)]
+
+
 def process_section(acc, lines):
+    ca = []
+    ping = []
     for line in lines:
         (check_patt(line, TIME_PATT, acc, ["Time"]) or
             check_patt(line, HEADING_PATT, acc, [("Deg", int)]) or
@@ -93,8 +148,53 @@ def process_section(acc, lines):
             check_patt(line, QENG_SA_PATT, acc, ["SC State", "SC Mode"]) or
             check_patt(line, QENG_LTE_PATT, acc, ["SC State", "SC Mode"]) or
             check_patt(line, QENG_WCDMA_PATT, acc, ["SC State", "SC Mode"]) or
-            check_patt(line, QRSRP_PATT, acc, [("PRX", int), ("DRX", int), ("RX2", int), ("RX3", int)])
+            check_patt(line, QRSRP_PATT, acc, [("PRX", int), ("DRX", int), ("RX2", int), ("RX3", int)]) or
+            check_patt(line, MODEPREF_PATT, acc, ["Mode Pref"]) or
+            check_patt(line, CREG_PATT, acc, [("Reg State", map_creg_state)]) or
+            check_patt(line, COPS_PATT, acc, ["Oper", ("AcT", map_act_state)]) or
+            check_patt(line, QENDC_PATT, acc, [("5G Icon", lambda v: int(v) == 1)]) or
+            check_patt(line, PING_2_PATT, acc, [("Ping Pkt Loss", int)])
             )
+
+        ca_obj = {}
+        if (check_patt(line, QCAINFO_PCC_PATT, ca_obj, ["Type", ("EARFCN", int), ("Bandwidth", map_lte_bandwidth),
+                                                        ("Band", int), ("PCell State", map_pcell_state), "PCID",
+                                                        ("RSRP", int), ("RSRQ", int), ("RSSI", int), ("SINR", int)]) or
+                check_patt(line, QCAINFO_SCC_PATT, ca_obj, ["Type", ("EARFCN", int), ("Bandwidth", map_scc_bandwidth),
+                                                        ("Band", int), ("SCell State", map_scell_state), "PCID",
+                                                        ("RSRP", int), ("RSRQ", int), ("RSSI", int), ("SINR", int)])):
+            ca.append(ca_obj)
+
+        ping_obj = {}
+        if check_patt(line, PING_1_PATT, ping_obj, [("P", float)]):
+            ping.append(ping_obj["P"])
+
+    acc["CA"] = ca
+    acc["CA Cnt"] = len(ca)
+    acc["CA Tot Bandwidth"] = sum([x["Bandwidth"] for x in ca])
+
+    ping_min = None
+    ping_max = None
+    ping_jitter = 0
+    ping_sum = 0
+    for i, sample in enumerate(ping):
+        if not ping_min or sample < ping_min:
+            ping_min = sample
+        if not ping_max or sample > ping_max:
+            ping_max = sample
+        ping_sum = ping_sum + sample
+        if i > 0:
+            ping_jitter = ping_jitter + abs(ping[i] - ping[i - 1])
+    acc["Ping Min"] = ping_min
+    acc["Ping Max"] = ping_max
+    if len(ping) > 0:
+        acc["Ping Avg"] = ping_sum / len(ping)
+    else:
+        acc["Ping Avg"] = None
+    if len(ping) > 1:
+        acc["Ping Jitter"] = ping_jitter / len(ping) - 1
+    else:
+        acc["Ping Jitter"] = None
 
     if acc.get("Time"):
         json.dump(acc, sys.stdout)
